5d471046ad6a2711c4f49c724d57f98dd159bc00
hoanganhminh01 <hoanganhminh01@gmail.com>
2022-09-08 19:59:59 +0700

    skeleton code

--- utils/data_analysis.py
+++ utils/data_analysis.py
@@ -1,3 +1,27 @@
 import cv2
 import numpy as np
 import sklearn as sk
+
+
+def compute_mean(image):
+    pass
+
+
+def compute_median(image):
+    pass
+
+
+def compute_std(image):
+    pass
+
+
+def compute_max(image):
+    pass
+
+
+def compute_min(image):
+    pass
+
+
+def compute_mode(image):
+    pass
