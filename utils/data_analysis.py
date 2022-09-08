e48bc1d4cc1b7c34d083a5de27e4726d1614f7ee
Quang-Nguyen Vo-Huynh <quangnguyenvohuynh@gmail.com>
2022-09-08 22:13:01 +0700

    Update data_analysis.py

--- utils/data_analysis.py
+++ utils/data_analysis.py
@@ -4,25 +4,25 @@ import sklearn as sk
 from scipy import stats
 
 
-def compute_mean(image):
-    return np.mean(image)
+def compute_mean(image: np.ndarray) -> float:
+    return np.mean(image.flatten())
 
 
-def compute_median(image):
-    return np.median(image)
+def compute_median(image: np.ndarray) -> float:
+    return np.median(image.flatten())
 
 
-def compute_std(image):
-    return np.std(image)
+def compute_std(image: np.ndarray) -> float:
+    return np.std(image.flatten())
 
 
-def compute_max(image):
-    return np.amax(image, axis=0)
+def compute_max(image: np.ndarray) -> float:
+    return np.amax(image.flatten(), axis = 0)
 
 
-def compute_min(image):
-    return np.amin(image, axis=0)
+def compute_min(image: np.ndarray) -> float:
+    return np.amin(image.flatten(), axis = 0)
 
 
-def compute_mode(image):
-    return stats.mode(image, axis=0)
+def compute_mode(image: np.ndarray) -> float:
+    return stats.mode(image.flatten(), axis = 0)
