9b80288ac1b1b55bd9ae4a1176c4b47f5346dd7a
Quang-Nguyen Vo-Huynh <quangnguyenvohuynh@gmail.com>
2022-10-02 13:38:39 +0700

    newest update

--- utils/exploratory_data_analysis.py
+++ utils/exploratory_data_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import stats
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 def compute_statistical_parameters(images: object) -> tuple[object, object, object, object, object, object]:
     """
@@ -33,7 +34,8 @@ def compute_average_image(images: object) -> object:
 
     return average_image
 
-def compute_image_embeddings(images: object, labels: object, types: object, n_of_components: int):
+def compute_image_embeddings_PCA(images: object, labels: object, types: object, n_of_components: int) \
+    -> tuple[object, list]:
     """
     @author: Vo, Huynh Quang Nguyen
     """
@@ -43,33 +45,49 @@ def compute_image_embeddings(images: object, labels: object, types: object, n_of
     
     embedding_classes = []
     for label, type in zip(list(labels), list(types)):
-        if label == 0.0 and type == 'mono':
-            embedding_class = 'mono-functional'
-        elif label == 0.0 and type == 'poly':
-            embedding_class = 'poly-functional'
-        elif label == 1.0 and type == 'mono':
-            embedding_class = 'mono-defective'
-        elif label == 1.0 and type == 'poly':
-            embedding_class = 'poly-defective'
-        elif (label == 0.3333333333333333 or label == 0.6666666666666666) and type == 'mono':
-            embedding_class = 'mono-marginally-defective'
-        elif (label == 0.3333333333333333 or label == 0.6666666666666666) and type == 'poly':
-            embedding_class = 'poly-marginally-defective'
+        if label == 0.0 and type == 'mono': # 'mono-functional'
+            embedding_class = 1
+        elif (label == 0.3333333333333333 or label == 0.6666666666666666) \
+            and type == 'mono': # 'mono-marginally-defective'
+            embedding_class = 2
+        elif label == 1.0 and type == 'mono': # 'mono-defective'
+            embedding_class = 3
+        elif label == 0.0 and type == 'poly': # 'poly-functional'
+            embedding_class = 4
+        elif (label == 0.3333333333333333 or label == 0.6666666666666666) \
+            and type == 'poly': # 'poly-marginally-defective'
+            embedding_class = 5
+        elif label == 1.0 and type == 'poly': # 'poly-defective'
+            embedding_class = 6
         embedding_classes.append(embedding_class)
 
     return embeddings, embedding_classes
 
-def compute_eigenimages(images: object, explained_variance_ratio: float):
+def compute_image_embeddings_tSNE(images: object, labels: object, types: object) -> tuple[object, list]:
     """
     @author: Vo, Huynh Quang Nguyen
     """
-    assert (explained_variance_ratio >= 0.0) and (explained_variance_ratio <= 1.0), \
-        print('The ratio must higher than 0.0 and lower than 1.1')
-
     flatten_images = np.array([image.ravel() for image in images])
-    pca = PCA(explained_variance_ratio, whiten = True) 
-    transformed_images = pca.fit_transform(flatten_images)
-    transformed_images = pca.inverse_transform(transformed_images)
+    tsne = TSNE(n_components = 2, random_state = 0)
+    embeddings = tsne.fit_transform(flatten_images)
 
-    return transformed_images
+    embedding_classes = []
+    for label, type in zip(list(labels), list(types)):
+        if label == 0.0 and type == 'mono': # 'mono-functional'
+            embedding_class = 1
+        elif (label == 0.3333333333333333 or label == 0.6666666666666666) \
+            and type == 'mono': # 'mono-marginally-defective'
+            embedding_class = 2
+        elif label == 1.0 and type == 'mono': # 'mono-defective'
+            embedding_class = 3
+        elif label == 0.0 and type == 'poly': # 'poly-functional'
+            embedding_class = 4
+        elif (label == 0.3333333333333333 or label == 0.6666666666666666) \
+            and type == 'poly': # 'poly-marginally-defective'
+            embedding_class = 5
+        elif label == 1.0 and type == 'poly': # 'poly-defective'
+            embedding_class = 6
+        embedding_classes.append(embedding_class)
+
+    return embeddings, embedding_classes
 
